ea0ad5c38c76f19375db494cfae3ec28d391f30d
msmahfuz3140 <mdmahfuzulhaque3140@gmail.com>
2026-05-12 04:33:05 +0600

    added metadat

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,33 @@ import HomeCards from '@/components/HomeCards';
 import BloodInfoTable from '@/components/BloodInfoTable';
 import Footer from '@/components/Footer';
 
+export const metadata = {
+  title: "ফ্রী ব্লাড ডোনার স্বেচ্ছাসেবী ফাউন্ডেশন",
+  description: "  রক্তদান করুন এবং জীবন বাঁচান মানুষের হাতে হাত দিয়ে। রক্তদাতা হোন আপনিও।",
+  keywords: "Blood Donation Practice Project, blood donation, blood donors, save lives, emergency blood, blood bank, donate blood, Bangladesh, রক্তদান, রক্তদাতা",
+  openGraph: {
+    title: "ফ্রী ব্লাড ডোনার স্বেচ্ছাসেবী ফাউন্ডেশন",
+    description: "  রক্তদান করুন এবং জীবন বাঁচান মানুষের হাতে হাত দিয়ে। রক্তদাতা হোন আপনিও।",
+    siteName: "ফ্রী ব্লাড ডোনার স্বেচ্ছাসেবী ফাউন্ডেশন",
+    url: 'https://blood-donation-practice-project.vercel.app',
+    images: [
+      {
+        url: '/images/blood-doctor.jfif',
+        width: 1200,
+        height: 630,
+        alt: '  রক্তদান করুন এবং জীবন বাঁচান মানুষের হাতে হাত দিয়ে। রক্তদাতা হোন আপনিও।',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ফ্রী ব্লাড ডোনার স্বেচ্ছাসেবী ফাউন্ডেশন',
+    description: '  রক্তদান করুন এবং জীবন বাঁচান মানুষের হাতে হাত দিয়ে। রক্তদাতা হোন আপনিও।',
+    site: '@BloodDonationProject',
+    images: ['/images/blood-doctor.jfif'],
+  },
+};
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50">
